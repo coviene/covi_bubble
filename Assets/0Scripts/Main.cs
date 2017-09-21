24fba2c77c7f441e95cfcd77a8ae60f5298519fe
khaled <khaled.dallah0@gmail.com>
2017-09-21 14:32:49 -0700

    Initial commit

---
+++ Assets/0Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+	public float speed;
+	private Vector3 mov1;
+	private float x;
+	private float z;
+	// Use this for initialization
+	void Start () {
+		mov1 = new Vector3 (0, 0, 0);
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		x = Input.GetAxis ("Horizontal");
+		z = Input.GetAxis ("Vertical");
+		mov1.Set (x, 0, z);
+		transform.Translate(mov1*speed*Time.deltaTime);
+	}
+}
---
+++ Assets/0Scripts/vars.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vars : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.localScale = new Vector3 (3, 3, 3);
+		Renderer r = cube.GetComponent<Renderer>();
+		r.material = new Material(Shader.Find("Transparent/Diffuse"));
+		//r.material.Color = new Color (1, 0, 0, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
